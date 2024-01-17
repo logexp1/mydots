2aa8d6c88895c2e7b8f030d29c8e662180686876
Jisoo Lee <jisoo.h.lee@gmail.com>
2024-01-17 07:13:14 -0500

    some error fixes

--- terminal/.zsh/stow_functions.sh
+++ terminal/.zsh/stow_functions.sh
@@ -1,10 +1,10 @@
 # as of stow 2.2.2, READMEs are automatically ignored
 restow() {
-	cd ~/fedoradots && \
+	cd ~/mydots && \
 		stow -Rvt ~/ emacs terminal remap scripts startup common browsing
 }
 
 unstow() {
-	cd ~/dotfiles && \
+	cd ~/mydots && \
 		stow -Dvt ~/ emacs terminal remap scripts startup common browsing
 }
