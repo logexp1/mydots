509e3cf63ff0432b6cd3c258483af0b973952521
Jisoo Lee <jisoo.h.lee@gmail.com>
2024-01-20 00:22:08 +0900

    some errors fixed and packages added

--- terminal/.zsh/stow_functions.sh
+++ terminal/.zsh/stow_functions.sh
@@ -1,10 +1,10 @@
 # as of stow 2.2.2, READMEs are automatically ignored
 restow() {
 	cd ~/mydots && \
-		stow -Rvt ~/ emacs terminal remap scripts startup common browsing
+		stow -Rvt ~/ emacs terminal remap scripts startup common browsing assets
 }
 
 unstow() {
 	cd ~/mydots && \
-		stow -Dvt ~/ emacs terminal remap scripts startup common browsing
+		stow -Dvt ~/ emacs terminal remap scripts startup common browsing assets
 }
