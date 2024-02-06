a43d9854df6e807f2f569e0caa1fcc1776c49efd
Jisoo Lee <jisoo.h.lee@gmail.com>
2024-02-06 22:59:11 +0900

    zathura

--- terminal/.zsh/stow_functions.sh
+++ terminal/.zsh/stow_functions.sh
@@ -1,10 +1,12 @@
 # as of stow 2.2.2, READMEs are automatically ignored
 restow() {
 	cd ~/mydots && \
-		stow -Rvt ~/ emacs terminal remap scripts startup common browsing assets wm
+		stow -Rvt ~/ emacs terminal remap scripts startup \
+		common browsing assets wm media
 }
 
 unstow() {
 	cd ~/mydots && \
-		stow -Dvt ~/ emacs terminal remap scripts startup common browsing assets wm
+		stow -Dvt ~/ emacs terminal remap scripts startup \
+		common browsing assets wm media
 }
